ee0d9b1a575e4f2971904bfe82228c52a9b83320
Olalekan Yusuf <olasyusuf@gmail.com>
2025-09-13 05:07:07 +0100

    chore: Creating Ambient Namespaces - fixing crud.d.ts functions' signature

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -6,13 +6,13 @@ declare namespace CRUD {
    * @param row The RowElement to insert.
    * @returns The ID of the newly inserted row.
    */
-  function insertRow(row: RowElement): RowID;
+  declare function insertRow(row: RowElement): number;
 
   /**
    * Deletes a row from the database.
    * @param rowId The ID of the row to delete.
    */
-  function deleteRow(rowId: RowID): void;
+  declare function deleteRow(rowId: RowID): void;
 
   /**
    * Updates an existing row in the database.
@@ -20,7 +20,7 @@ declare namespace CRUD {
    * @param row The updated RowElement data.
    * @returns The ID of the updated row.
    */
-  function updateRow(rowId: RowID, row: RowElement): RowID;
+  declare function updateRow(rowId: RowID, row: RowElement): RowID;
 }
 
 export default CRUD;
